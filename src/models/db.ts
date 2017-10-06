b325c6a646c9b9c42fd72668c0d805e0035f1aa7
Lasse Sprengel <lasse.sprengel@protonmail.ch>
2017-10-06 16:33:40 +0200

    Initial Commit

---
+++ src/models/db.ts
@@ -0,0 +1,26 @@
+import * as mongoose from 'mongoose';
+
+// tslint:disable-next-line:no-var-requires
+const dbURI = require(__dirname + '/../../config.json').db.uri;
+
+mongoose.connect(dbURI, {
+  useMongoClient: true,
+  /* other options */
+});
+
+mongoose.connection.on('connected', () => {
+  console.log('Mongoose connected to ' + dbURI);
+});
+mongoose.connection.on('error', (err) => {
+  console.error('Mongoose connection error: ' + err);
+  process.exit(1);
+});
+mongoose.connection.on('disconnected', () => {
+  console.error('Mongoose disconnected');
+  process.exit(1);
+});
+
+// #################################
+// Import all mongo DB models here
+// #################################
+// import './model-name';

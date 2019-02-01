00f0f9d76fac29e56013abb8a1bec7676c85d62e
Lasse Sprengel <lasse.sprengel@protonmail.ch>
2019-02-01 13:54:31 +0100

    Security: Removed log containing registration data

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -11,7 +11,6 @@ export const register = [
     check('password').isLength({ min: 8 }),
     validation,
     (req: Request, res: Response) => {
-        console.log('register', req.body.username, req.body.password);
         const user = new User() as IUser;
         user.name = req.body.username;
         user.setPassword(req.body.password, () => {
